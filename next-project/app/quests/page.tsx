56e24df4e0f5fb3f68f9c202962a256ae950442f
rubenb1604 <rubenbertram@gmx.de>
2024-11-15 23:01:31 +0100

    q2

--- next-project/app/quests/page.tsx
+++ next-project/app/quests/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/header2";
 import Quest from "@/components/quest";
+import Quest2 from "@/components/quest2";
 
 const quests = () => {
 	return (
 		<div className="mt-32">
 			<Header />
+			<Quest2 />
 			<Quest />
 		</div>
 	);
---
+++ next-project/components/quest2.tsx
@@ -0,0 +1,40 @@
+const Quest2 = () => {
+	return (
+		<div className="h-fit bg-neutral-700 m-16 p-8 rounded-xl border-yellow-500 border-4">
+			<span className="text-white font-sans font-extrabold text-4xl">
+				Quest of the Day:{" "}
+			</span>
+			<span className="text-yellow-500 font-sans font-extrabold text-4xl">
+				#002
+			</span>
+			<p className="pt-12 text-neutral-200 text-xl font-bold">
+				Laufe vom<span className="text-blue-400"> Spawn </span>ca.
+				<span className="text-blue-400"> 600 Blöcke </span>in Richtung
+				<span className="text-blue-400"> Norden</span>. Auf der
+				<span className="text-blue-400"> Spitze des Berges </span>erwartet dich
+				in seiner <span className="text-blue-400">kleinen Hütte</span> ein
+				netter <span className="text-blue-400">Dorfbewohner</span>. Dieser wird
+				dir weiterhelfen. Nachdem du bei seinem
+				<span className="text-blue-400"> Ziel</span> bis
+				<span className="text-blue-400"> hoch aufs Dach </span>
+				geklettert bist, gehe ca.
+				<span className="text-blue-400"> 450 Blöcke</span> in Richtung des
+				<span className="text-blue-400"> Pfeiles</span>. Hier findest du neben
+				der <span className="text-blue-400">Markierung</span> einen
+				<span className="text-blue-400"> merkwürdigen Baum</span>. Unter diesem
+				verbirgt sich der <span className="text-blue-400">Schatz</span>! Auch
+				hier ist für jeden Spieler
+				<span className="text-blue-400"> genau ein Slot </span>
+				des Fasses vorgesehen. Wer mehr nimmt,
+				<span className="text-blue-400"> verstößt gegen die Regeln </span>
+				und muss mit <span className="text-blue-400"> Konsequenzen </span>
+				rechnen.
+				<br />
+				<br />
+				<span className="text-gray-400">Tipp: Nimm ein paar Smaragte mit!</span>
+			</p>
+		</div>
+	);
+};
+
+export default Quest2;
